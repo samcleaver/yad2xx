51e33bdf2224489f90d2da36af80a41f0a508a13
sjdavies <sjdavies@c621fee0-20a5-47c1-9051-b0b0a60408e0>
2015-02-05 10:58:40 +0000

    Improve EEPROMDumpTest.

--- yad2xxJava/src/test/java/net/sf/yad2xx/EEPROMDumpTest.java
+++ yad2xxJava/src/test/java/net/sf/yad2xx/EEPROMDumpTest.java
@@ -12,11 +12,11 @@ public class EEPROMDumpTest {
 
 	/**
 	 * EEPROM size in bytes.
-	 * Defaults to 93C46 (1k).
+	 * Defaults to 93C46 (128x8).
 	 */
-	private static int PROM_SIZE = 1024;
+	private static final int PROM_SIZE = 128;
 	
-	private static String HEX = "0123456789ABCDEF";
+	private static final String HEX = "0123456789ABCDEF";
 	
 	/**
 	 * @param args
@@ -30,29 +30,72 @@ public class EEPROMDumpTest {
 			Device dev = ftdi.getDevices()[0];
 			
 			try {
+				System.out.println("Found: " + dev.getDescription());
 				dev.open();
 				
+				StringBuilder characters = null;
+				
 				for (int i = 0; i < (PROM_SIZE / 2);) {
-					int word = dev.readEE(i++);
-					printHex(System.out, word & 0xff);
-					System.out.print(' ');
-					printHex(System.out, (word >> 8) & 0xff);
-
+					
+					// print byte counts
 					if (i % 8 == 0) {
-						System.out.println();
-					} else if (i % 4 == 0) {
+						if (i != 0) {
+							// suppress first time
+							System.out.println(characters.toString());
+						}
+						printHex(System.out, (i >> 7) & 0xff);
+						printHex(System.out, (i << 1) & 0xff);
+						System.out.print(' ');
+						characters = new StringBuilder();
+					}
+					
+					int word = dev.readEE(i++);
+					int lowByte = word & 0xff;
+					int highByte = (word >> 8) & 0xff;
+					
+					// print hex values
+					printHex(System.out, lowByte);
+					System.out.print(' ');
+					printHex(System.out, highByte);
+					
+					// Accumulate ascii characters
+					characters.append(mapAscii(lowByte));
+					characters.append(mapAscii(highByte));
+					
+					if ((i % 4 == 0) && (i % 8 != 0)) {
 						System.out.print('-');
+						characters.append('-');
 					} else {
 						System.out.print(' ');
 					}
 				}
 			}
+			catch (FTDIException e) {
+				e.printStackTrace(System.err);
+				System.err.println("In FTDI function: " + e.getFunction());
+			}
 			finally {
 				dev.close();
 			}
+		} else {
+			System.err.println("No device found");
 		}
 	}
 
+	/**
+	 * Convert an ASCII byte value to its printable value. Non-printable values appear as dots.
+	 * 
+	 * @param value
+	 * @return
+	 */
+	private static char mapAscii(int value) {
+		if ((value < 0x20) || (value > 0x7F)) {
+			return '.';
+		} else {
+			return (char) (value & 0x7F);
+		}
+	}
+	
 	private static void printHex(PrintStream out, int value) {
 		out.print(HEX.charAt(value / 16));
 		out.print(HEX.charAt(value & 0xf));
